c89f90f903f7798b22a137cb0c75c0f37cfee4aa
unknown <enoquezh@gmail.com>
2021-09-26 19:30:57 -0300

    Exemplos com pre e pos incremento

--- estruturas de repeticao/ExemploRepeticao.java
+++ estruturas de repeticao/ExemploRepeticao.java
@@ -3,27 +3,49 @@ import java.util.Random;
 public class ExemploRepeticao{
 
     public static void main(String[] args) {
-        Random gerador = new Random();
+
+        int cont =1;
+        while (++cont <=1){ 
+            System.out.println("Outra interação:" + cont);
+        }
+        System.out.println("Fora do while:" + cont);
+/*         Random gerador = new Random();
         double nota1, nota2;
+        int totalAlunos = 10;
+        do{
+            nota1 = gerador.nextDouble() *10;
+            nota2 = gerador.nextDouble() *10;
+            System.out.printf("Média %.2f\n", (nota1 + nota2)/ 2);
+            ++totalAlunos;
+        }while (totalAlunos <= 10); */
+
+/*         int nota = 7;
+        //nota++
+        //pos-incremento
+        int resultadoFinal = nota++ + 1;
+        //pre-incremento
+        //int resultadoFinal = ++nota + 1; 
+        System.out.println(resultadoFinal); */
+
         //inicialização (incluindo, a declaração)
         //teste de continuidade
         //incremento
-        for( int totalAlunos = 1; totalAlunos <=5 ; totalAlunos++) { // int dentro só do for // totalAlunos +=2 de dois em dois
-            nota1 = gerador.nextDouble() *10;
-            nota2 = gerador.nextDouble() *10;
-            System.out.println(
-                String.format("Média: %.2f",  (nota1 + nota2) /2)
-            );
-        }
-        int totalAlunos =1;
-        while(totalAlunos <= 5){
-            nota1 = gerador.nextDouble() *10;
-            nota2 = gerador.nextDouble() *10;
-            System.out.println(
-                String.format("Média: %.2f",  (nota1 + nota2) /2)
-            );
-            totalAlunos++; // + = 2;
-        }
+/*             for( int totalAlunos = 1; totalAlunos <=5 ; totalAlunos++) { // int dentro só do for // totalAlunos +=2 de dois em dois
+                nota1 = gerador.nextDouble() *10;
+                nota2 = gerador.nextDouble() *10;
+                System.out.println(
+                    String.format("Média: %.2f",  (nota1 + nota2) /2)
+                );
+            }
+            int totalAlunos =1;
+            while(totalAlunos <= 5){
+                nota1 = gerador.nextDouble() *10;
+                nota2 = gerador.nextDouble() *10;
+                System.out.println(
+                    String.format("Média: %.2f",  (nota1 + nota2) /2)
+                );
+                totalAlunos++; // + = 2;
+            } */
         ////repetição controlada por contador
         //int totalAlunos = 1;
         ////içamento
